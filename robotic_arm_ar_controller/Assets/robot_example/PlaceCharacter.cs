0369430c7754c375da0dea4e9baec526f585b5c1
akileswaran <akileswaran.karthikeyan@gmail.com>
2020-07-15 16:59:48 +0200

    Init

---
+++ robotic_arm_ar_controller/Assets/robot_example/PlaceCharacter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceCharacter : MonoBehaviour
+{
+    public void Placecharacter()
+    {
+        transform.localPosition = Vector3.zero;
+    }
+}
